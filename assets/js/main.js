aa201eded9554d4f9ef80131eee743129975d5c0
Babera91 <ncis@hotmail.de>
2021-10-07 23:29:39 +0200

    Rework

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+/* 
+Font: Lora/ Work sans
+font-family: 'Lora', serif;
+font-family: 'Work Sans', sans-serif;
+
+Farbe: #2e2e2e / #fff / #000 / #1d2c2f 
+
+https://www.youtube.com/watch?v=GI71fXmRrLM
+
+
+
+mit JS ein console.log von dem User-Input im Formular
+
+Media Queries auf 600px width
+
+*/
